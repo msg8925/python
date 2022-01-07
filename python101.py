53bcebae191595a7741cc696b8834b087bf731bc
unknownAuthor <msg.8925@gmail.com>
2022-01-07 03:14:19 -0500

    Added a simple function.

--- python101.py
+++ python101.py
@@ -9,3 +9,12 @@ print("\n")
 #print second string
 print("It's so lonely here...")
 
+#functions
+print("This function is about to execute.\n")
+def who_am_i():
+    name = "Ivan"
+    age = 34
+    print("My name is " + name + " and I am " + str(age) + " years old.")
+
+
+who_am_i()
