09943a3db56beff6cc6f62a3e079628cab862832
unknownAuthor <msg.8925@gmail.com>
2022-01-07 03:51:47 -0500

    Added a simple list.

--- python101.py
+++ python101.py
@@ -1,20 +1,32 @@
 #! /bin/python3
 
 #print string
-print("Hello world.")
+#print("Hello world.")
 
 #new line
-print("\n")
+#print("\n")
 
 #print second string
-print("It's so lonely here...")
+#print("It's so lonely here...")
 
 #functions
-print("This function is about to execute.\n")
-def who_am_i():
-    name = "Ivan"
-    age = 34
-    print("My name is " + name + " and I am " + str(age) + " years old.")
+#print("This function is about to execute.\n")
+#def who_am_i():
+#    name = "Ivan"
+#    age = 34
+#    print("My name is " + name + " and I am " + str(age) + " years old.")
 
+#who_am_i()
 
-who_am_i()
+#function with params
+#def who_are_you(name, age):
+
+#    print("Your name is " + name + " and your age is " + str(age) + " years old.")
+
+#who_are_you("David", 32)
+
+#lists
+friends = ["Joey", "Monica", "Chandler", "Ross", "Rachel", "Phoebe"]
+
+print(friends[0])
+print(friends[0:2])
